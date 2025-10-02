a9cf77ea4c9d08276e628723eb2954eb7159f5d8
RITESH MALIK <126045289+riteshmalik95@users.noreply.github.com>
2025-10-02 17:56:30 +0530

    Update DemoTest.java

--- src/main/java/WebTableDemo/DemoTest.java
+++ src/main/java/WebTableDemo/DemoTest.java
@@ -4,5 +4,7 @@ public class DemoTest {
     public static void main(String[] args) {
         System.out.println("i am an Automation tester");
         System.out.println("i am an API Automation tester");
+        System.out.println("i am an Selenium Tester");
+        
     }
 }
